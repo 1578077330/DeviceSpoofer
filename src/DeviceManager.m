3de62149940d7c2fa419b83552f62118cf9e4edf
1578077330 <1578077330@qq.com>
2025-09-26 18:42:32 +0800

    Update DeviceManager.m

--- src/DeviceManager.m
+++ src/DeviceManager.m
@@ -1,68 +1,21 @@
-#import "DeviceManager.h"
-#import <objc/runtime.h>
-#import <IOKit/IOKitLib.h>
-
-@implementation DeviceManager
-
-+ (instancetype)sharedManager {
-    static DeviceManager *sharedInstance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedInstance = [[self alloc] init];
-    });
-    return sharedInstance;
-}
-
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        [self loadSavedDeviceInfo];
-    }
-    return self;
-}
-
-- (void)spoofDeviceInfo {
-    NSLog(@"[DeviceSpoofer] 开始伪装设备信息...");
-    [self randomizeDeviceInfo];
-    [self applyDeviceInfo];
-}
-
+// 在randomizeDeviceInfo方法中添加更多随机化选项
 - (void)randomizeDeviceInfo {
-    // 生成随机设备信息
+    NSLog(@"[DeviceSpoofer] 开始生成随机设备信息...");
+    
+    // 设备型号列表
+    NSArray *deviceModels = @[
+        @"iPhone14,5", // iPhone 13
+        @"iPhone14,2", // iPhone 13 Pro
+        @"iPhone15,2", // iPhone 14 Pro
+        @"iPhone15,3", // iPhone 14 Pro Max
+        @"iPhone16,1"  // iPhone 15 Pro
+    ];
+    
+    // 随机选择设备型号
+    self.spoofedModel = deviceModels[arc4random_uniform((uint32_t)deviceModels.count)];
     self.spoofedUDID = [self generateRandomUDID];
     self.spoofedSerialNumber = [self generateRandomSerial];
-    self.spoofedModel = @"iPhone14,5"; // iPhone 13
-    self.spoofedProductName = @"iPhone13,1";
-    self.spoofedBoardId = @"D63AP";
+    self.spoofedProductName = [self.spoofedModel stringByReplacingOccurrencesOfString:@"," withString:@""];
     
-    NSLog(@"[DeviceSpoofer] 生成新设备信息: %@", self.spoofedUDID);
+    NSLog(@"[DeviceSpoofer] 新设备信息: %@, UDID: %@", self.spoofedModel, self.spoofedUDID);
 }
-
-- (NSString *)generateRandomUDID {
-    // 生成随机UDID
-    return [NSUUID UUID].UUIDString;
-}
-
-- (NSString *)generateRandomSerial {
-    // 生成随机序列号
-    return [NSString stringWithFormat:@"F%dLQ%dSJ", arc4random_uniform(1000), arc4random_uniform(10000)];
-}
-
-- (void)applyDeviceInfo {
-    NSLog(@"[DeviceSpoofer] 应用设备信息...");
-    // 这里会通过runtime hook系统方法
-}
-
-- (void)resetDeviceInfo {
-    NSLog(@"[DeviceSpoofer] 重置设备信息");
-    self.spoofedUDID = nil;
-    self.spoofedSerialNumber = nil;
-    self.spoofedModel = nil;
-    // 恢复原始设备信息
-}
-
-- (void)loadSavedDeviceInfo {
-    // 加载保存的设备信息
-}
-
-@end
